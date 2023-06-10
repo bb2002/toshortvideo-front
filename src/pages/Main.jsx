bd6336b1f7e589f030ff5b6113ca5deffe97b5c2
Ballbot <5252bb@daum.net>
2023-06-10 23:49:48 +0900

    업로드 API 연동 완성

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,7 +1,8 @@
+import { useState } from "react";
 import { styled } from "styled-components";
 import HeaderComp from "../components/Header";
-import { Typography, message, Upload } from "antd";
-import { InboxOutlined } from '@ant-design/icons';
+import { Alert, Typography, Upload } from "antd";
+import { VideoCameraAddOutlined } from '@ant-design/icons';
 
 const { Dragger } = Upload;
 
@@ -20,48 +21,70 @@ const Headline = styled.div`
 		color: #5A5A5A;
 		font-size: 0.82rem;
 	}
-`
+`;
 
-const props = {
-  name: 'file',
-  multiple: true,
-  action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
-  onChange(info) {
-    const { status } = info.file;
-    if (status !== 'uploading') {
-      console.log(info.file, info.fileList);
-    }
-    if (status === 'done') {
-      message.success(`${info.file.name} file uploaded successfully.`);
-    } else if (status === 'error') {
-      message.error(`${info.file.name} file upload failed.`);
-    }
-  },
-  onDrop(e) {
-    console.log('Dropped files', e.dataTransfer.files);
-  },
-};
 
 const MainPage = () => {
+	const [errorAlert, setErrorAlert] = useState(null);
+	const [uploadingFile, setUploadingFile] = useState(null);
+	const [uploadedFiles, setUploadedFiles] = useState([]);
+
+	const props = {
+		action: 'https://tsvapi.blbt.app/editor/upload',
+		multiple: true,
+		method: 'PUT',
+		name: 'file',
+		onChange(info) {
+			const { status } = info.file;
+
+			if (status === 'uploading') {
+				setUploadingFile(info.file);
+				setErrorAlert(null);
+			} else {
+				if (status === 'done') {
+					setUploadedFiles([...uploadedFiles, info.file]);
+				}
+	
+				if (status === 'error') {
+					setErrorAlert('동영상을 업로드하지 못했습니다.');
+				}
+
+				setUploadingFile(null);
+			}
+		}
+	}
+
   return (
 		<>
 			<HeaderComp />
+			<div className="container">
+				<Headline>
+					<Typography.Title level={1} id="headline-title">세상에서 가장 빠르게 숏츠 영상 만들기</Typography.Title>
+					<p id="headline-content">1분짜리 영상 만드는데 편집만 20분? 이제 영상 업로드하고 글자만 바꿔 숏츠 영상을 찍어내세요.</p>
+				</Headline>
 
-			<Headline>
-				<Typography.Title level={1} id="headline-title">세상에서 가장 빠르게 숏츠 영상 만들기</Typography.Title>
-				<p id="headline-content">1분짜리 영상 만드는데 편집만 20분? 이제 영상 업로드하고 글자만 바꿔 숏츠 영상을 찍어내세요.</p>
-			</Headline>
+				{
+					errorAlert && <>
+						<Alert message={errorAlert} type="error" showIcon style={{ marginBottom: 32 }} />
+					</>
+				}
 
-			<Dragger {...props}>
-				<p className="ant-upload-drag-icon">
-					<InboxOutlined />
-				</p>
-				<p className="ant-upload-text">Click or drag file to this area to upload</p>
-				<p className="ant-upload-hint">
-					Support for a single or bulk upload. Strictly prohibited from uploading company data or other
-					banned files.
-				</p>
-			</Dragger>
+				{
+					uploadedFiles.map((uploadedFile) => (<>
+						<p>{uploadedFile.response.uuid}</p>
+					</>))
+				}
+
+				<Dragger {...props} fileList={uploadingFile ? [uploadingFile] : []}>
+					<p className="ant-upload-drag-icon">
+						<VideoCameraAddOutlined />
+					</p>
+					<p className="ant-upload-text">이곳에 편집할 동영상을 드래그하세요.</p>
+					<p className="ant-upload-hint">
+						mp4, mkv 확장의 파일을 지원하며, 업로드된 파일은 상단 '개발팀'의 업로드 정책을 따릅니다.
+					</p>
+				</Dragger>
+			</div>
 		</>
 	)
 }
