af6b86828ee2900ff05eb634d471dd00b8fbe022
Ballbot <5252bb@daum.net>
2023-06-13 13:41:55 +0000

    VideoEditor 는 일단 완성

--- src/components/VideoEditor.jsx
+++ src/components/VideoEditor.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Form, Typography, Input, Select } from 'antd'
+import { Form, Typography, Input, Select, Space } from 'antd'
 import { InfoCircleOutlined } from '@ant-design/icons'
 import { styled } from 'styled-components'
 
@@ -78,23 +78,23 @@ const EditorInputGroup = styled.div`
 const VideoEditor = ({ data }) => {
   const [recipe, setRecipe] = useState({
     text1: {
-      text: '진짜제발되라진짜',
-      color: '#F5FF15',
+      text: 'TEXT1',
+      color: '#FF0000',
       font: 'NotoSansKR',
       weight: 'Bold',
-      fontSize: 0.12
+      fontSize: 0.1
     },
     text2: {
-      text: '제발되라하진짜제발',
-      color: '#FFFFFF',
+      text: 'TEXT2',
+      color: '#FF0000',
       font: 'NotoSansKR',
       weight: 'Bold',
       fontSize: 0.1
     },
     video: {
-      startAt: 5,
-      endAt: 17,
-      blankFill: 'Blur',
+      startAt: 0,
+      endAt: data.videoDuration,
+      blankFill: 'Black',
       videoSize: '0.65'
     }
   })
@@ -104,15 +104,8 @@ const VideoEditor = ({ data }) => {
     const newRecipe = { ...recipe }
     newRecipe[depth1][depth2] = target.value
     setRecipe(newRecipe)
-  }
 
-  const onText1FontChanged = (value) => {
-    onInputChanged({
-      target: {
-        id: 'text1.font',
-        value
-      }
-    })
+    console.log(newRecipe)
   }
 
   return (
@@ -120,8 +113,8 @@ const VideoEditor = ({ data }) => {
 			<PreviewContainer>
 				<PreviewBackgroundContainer style={{ backgroundImage: 'url("' + data.thumbnailUrl + '")' }} />
 				<PreviewTextContainer>
-					<PreviewText style={{ color: 'white' }}>이게 어떻게 힐러임 탱커 때려잡는 브리기테</PreviewText>
-					<PreviewText style={{ color: 'white' }}>TEXT2</PreviewText>
+					<PreviewText style={{ color: recipe.text1.color }}>{recipe.text1.text}</PreviewText>
+					<PreviewText style={{ color: recipe.text2.color }}>{recipe.text2.text}</PreviewText>
 				</PreviewTextContainer>
 				<PreviewImgContainer>
 					<img src={data.thumbnailUrl} />
@@ -149,11 +142,113 @@ const VideoEditor = ({ data }) => {
 							<Input id="text1.fontSize" type="range" min={0.05} max={0.15} step={0.01} value={recipe.text1.fontSize} onChange={onInputChanged} />
 						</Form.Item>
 					</Form>
-					<Form layout="vertical" style={{ flex: 0.5, marginLeft: 8 }}>
+					<Form layout="vertical" style={{ flex: 0.75, marginLeft: 8 }}>
 						<Form.Item label="상단 텍스트 폰트">
-							<Select defaultValue="NotoSansKR" onSelect={onText1FontChanged}>
-								<Select.Option value="NotoSansKR">NotoSansKR</Select.Option>
-								<Select.Option value="SCDream">SCDream</Select.Option>
+							<Space.Compact style={{ width: '100%' }}>
+								<Select defaultValue="NotoSansKR" value={recipe.text1.font} onSelect={(value) => onInputChanged({
+								  target: {
+								    id: 'text1.font',
+								    value
+								  }
+								})}>
+									<Select.Option value="NotoSansKR">NotoSansKR</Select.Option>
+									<Select.Option value="SCDream">SCDream</Select.Option>
+								</Select>
+								<Select defaultValue="Bold" value={recipe.text1.weight} onSelect={(value) => onInputChanged({
+								  target: {
+								    id: 'text1.weight',
+								    value
+								  }
+								})}>
+									<Select.Option value="Bold">Bold</Select.Option>
+									<Select.Option value="">Normal</Select.Option>
+									<Select.Option value="Light">Light</Select.Option>
+								</Select>
+							</Space.Compact>
+						</Form.Item>
+					</Form>
+				</EditorInputGroup>
+
+				<EditorInputGroup>
+					<Form layout="vertical" requiredMark="optional" style={{ flex: 1.5 }}>
+						<Form.Item
+							label="하단 텍스트"
+							tooltip={{ title: '최대 20자 까지 입력 가능', icon: <InfoCircleOutlined /> }}>
+							<Input id="text2.text" type="text" placeholder="하단 텍스트 입력" maxLength={20} onChange={onInputChanged} value={recipe.text2.text} />
+						</Form.Item>
+					</Form>
+					<Form layout="vertical" style={{ flex: 0.5, marginLeft: 8 }}>
+						<Form.Item label="하단 텍스트 색상">
+							<Input id="text2.color" type="color" onChange={onInputChanged} value={recipe.text2.color} />
+						</Form.Item>
+					</Form>
+					<Form layout="vertical" style={{ flex: 0.5, marginLeft: 8 }}>
+						<Form.Item label="하단 텍스트 크기">
+							<Input id="text2.fontSize" type="range" min={0.05} max={0.15} step={0.01} value={recipe.text2.fontSize} onChange={onInputChanged} />
+						</Form.Item>
+					</Form>
+					<Form layout="vertical" style={{ flex: 0.75, marginLeft: 8 }}>
+						<Form.Item label="하단 텍스트 폰트">
+							<Space.Compact style={{ width: '100%' }}>
+								<Select defaultValue="NotoSansKR" value={recipe.text2.font} onSelect={(value) => onInputChanged({
+								  target: {
+								    id: 'text2.font',
+								    value
+								  }
+								})}>
+									<Select.Option value="NotoSansKR">NotoSansKR</Select.Option>
+									<Select.Option value="SCDream">SCDream</Select.Option>
+								</Select>
+								<Select defaultValue="Bold" value={recipe.text2.weight} onSelect={(value) => onInputChanged({
+								  target: {
+								    id: 'text2.weight',
+								    value
+								  }
+								})}>
+									<Select.Option value="Bold">Bold</Select.Option>
+									<Select.Option value="">Normal</Select.Option>
+									<Select.Option value="Light">Light</Select.Option>
+								</Select>
+							</Space.Compact>
+						</Form.Item>
+					</Form>
+				</EditorInputGroup>
+
+				<EditorInputGroup>
+					<Form layout="vertical" style={{ flex: 0.5 }}>
+						<Form.Item label="비디오 시작 시간">
+							<Input type="number" id="video.startAt" addonAfter="초" defaultValue={0} min={0} max={data.videoDuration} step={0.1} onChange={onInputChanged} />
+						</Form.Item>
+					</Form>
+					<Form layout="vertical" style={{ flex: 0.5, marginLeft: 8 }}>
+						<Form.Item label="비디오 종료 시간">
+							<Input type="number" id="video.endAt" addonAfter="초" defaultValue={data.videoDuration > 60 ? 60 : data.videoDuration} min={0} max={data.videoDuration} step={0.1} onChange={onInputChanged} />
+						</Form.Item>
+					</Form>
+					<Form layout="vertical" style={{ flex: 0.5, marginLeft: 8 }}>
+						<Form.Item label="배경 처리">
+							<Select defaultValue="Black" value={recipe.video.blankFill} onSelect={(value) => onInputChanged({
+							  target: {
+							    id: 'video.blankFill',
+							    value
+							  }
+							})}>
+								<Select.Option value="Black">검정</Select.Option>
+								<Select.Option value="Blur">블러</Select.Option>
+							</Select>
+						</Form.Item>
+					</Form>
+					<Form layout="vertical" style={{ flex: 0.5, marginLeft: 8 }}>
+						<Form.Item label="동영상 표시 크기">
+							<Select defaultValue="0.65" value={recipe.video.videoSize} onSelect={(value) => onInputChanged({
+							  target: {
+							    id: 'video.videoSize',
+							    value
+							  }
+							})}>
+								<Select.Option value="0.5">작게</Select.Option>
+								<Select.Option value="0.65">보통</Select.Option>
+								<Select.Option value="1.0">꽉 차게</Select.Option>
 							</Select>
 						</Form.Item>
 					</Form>
