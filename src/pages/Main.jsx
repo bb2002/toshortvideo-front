23e109806692ca3d9a587bf4412a4b0da76daa7c
Ballbot <5252bb@daum.net>
2023-06-14 13:14:01 +0000

    폰트 등록완료, 에디터 얼추 완성

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react'
 import { styled } from 'styled-components'
 import HeaderComp from '../components/Header'
-import { Alert, Collapse, Typography, Upload } from 'antd'
-import { VideoCameraAddOutlined } from '@ant-design/icons'
+import { Alert, Button, Collapse, Typography, Upload } from 'antd'
+import { VideoCameraAddOutlined, RocketOutlined } from '@ant-design/icons'
 import VideoEditor from '../components/VideoEditor'
 
 const { Dragger } = Upload
@@ -24,10 +24,29 @@ const Headline = styled.div`
 	}
 `
 
+const ButtonContainer = styled.div`
+	width: 100%;
+	margin-top: 16px;
+	margin-bottom: 64px;
+`
+
 const MainPage = () => {
   const [errorAlert, setErrorAlert] = useState(null)
   const [uploadingFile, setUploadingFile] = useState(null)
-  const [uploadedFiles, setUploadedFiles] = useState([])
+  const [uploadedFiles, setUploadedFiles] = useState([{
+    response: {
+      uuid: '22c61ea6-789c-4de0-a447-20f1f8b87c60',
+      originalVideoName: 'video.mp4',
+      videoSize: 1128375,
+      videoMimetype: 'video/mp4',
+      videoDuration: 5.055,
+      downloadUrl: 'https://toshortvideo.d472b93232e7d3b8189d494a36bc92a8.r2.cloudflarestorage.com/uploadedVideos/22c61ea6-789c-4de0-a447-20f1f8b87c60?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=2ed46946fc06273087b504258cf4a3bf%2F20230603%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230603T085539Z&X-Amz-Expires=86400&X-Amz-Signature=931692b492cc4496ff38ee2b1c45823c8f3c89cc9453c90059f5732dc3eb9439&X-Amz-SignedHeaders=host',
+      thumbnailUrl: 'https://toshortvideo.d472b93232e7d3b8189d494a36bc92a8.r2.cloudflarestorage.com/thumbnailImages/408db8f7-9f59-4ef8-a3c8-8ecfb1391853?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=2ed46946fc06273087b504258cf4a3bf%2F20230603%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230603T085539Z&X-Amz-Expires=86400&X-Amz-Signature=8dc723c790335c31e35e9c03b334935b6c752c0dc64d91903404a60c3f3c1155&X-Amz-SignedHeaders=host',
+      expiredAt: '2023-06-04T08:55:39.827Z',
+      id: 2,
+      createdAt: '2023-06-02T23:55:39.856Z'
+    }
+  }])
 
   const props = {
     action: 'https://tsvapi.blbt.app/editor/upload',
@@ -76,6 +95,13 @@ const MainPage = () => {
 						  label: response.originalVideoName,
 						  children: <VideoEditor data={response} />
 						}))} />
+						<ButtonContainer>
+							<Button type="primary" icon={<RocketOutlined />} size="large" style={{ marginLeft: 'auto' }}>
+								인코딩 시작
+							</Button>
+						</ButtonContainer>
+
+						<div style={{ height: 40 }} />
 					</>
 				}
 
